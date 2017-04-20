6661a4f209ca86a842e954d0125f88028aba3281
605468025@qq.com <nihaoma123>
2017-04-20 11:25:15 +0800

    123

---
+++ member_collect.php
@@ -0,0 +1,30 @@
+<?php 
+	session_start();
+	/**
+	 * 我的账户-我的收藏
+	 */
+	require('plugins.php');
+	
+	
+	
+	if(!empty($_SESSION['_shop_login_id'])){
+		$smarty = new Smarty;
+		
+		$smarty->debugging = _debugging;
+		$smarty->caching = _caching;
+		$smarty->cache_lifetime = _cache_lifetime;
+		
+		$smarty->assign("dir",WEB_PATH);
+		$smarty->assign("head_title",'我的账户-我的收藏-生鲜网 '.WEB_NAME);
+		$smarty->assign("WEB_KEYWORD",WEB_KEYWORD);
+		$smarty->assign("WEB_DESCRIBE",WEB_DESCRIBE);
+		
+		$smarty->assign("member_left_class3",'class="account_feature feature_choose"');
+		
+		require('_session.php');
+		$smarty->display('core/templates/member_collect.tpl',md5($_SERVER["REQUEST_URI"]));
+	}else{
+		echo '<script>window.location.href="login.php";</script>';	
+	}
+	
+?>
